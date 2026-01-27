48ebece6c042a69e401f0d4337bc2dca9e10241c
Shuō Matsumoto <121488477+shuomatsumoto@users.noreply.github.com>
2026-01-27 15:18:56 +0900

    Update PointEconomy.tsx

--- src/PointEconomy.tsx
+++ src/PointEconomy.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { supabase } from './lib/supabaseClient';
 
 type CurrencyRow = {
